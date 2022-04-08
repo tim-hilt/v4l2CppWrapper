bd613ebb3d4b459c692dbfedbf8c72e29791fa5c
Tim Hilt <thilt@in-technology.eu>
2022-04-08 15:36:13 +0200

    main: Add vars and clang-tidy info

--- main.cpp
+++ main.cpp
@@ -1,3 +1,11 @@
+#include <vector>
+
 #include "v4l2_capture.hpp"
 
-int main() { v4l2Capture::say_hello(); }
+int main() {
+  int test = 5;
+  std::size_t t = 0;
+  std::vector<bool> vec = std::vector<bool>();
+  vec.emplace_back(true);
+  v4l2Capture::say_hello();
+}
